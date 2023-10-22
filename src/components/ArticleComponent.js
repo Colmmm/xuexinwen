4c5d5c9df4e135e97161d61a4b5d54ff8a7cf9b1
Colm Sam <colmwaikeat@gmail.com>
2023-10-22 16:04:03 +0800

    added ArticleComponent, basic code to retrieve articles from backend

---
+++ src/components/ArticleComponent.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+
+const ArticleComponent = ({ articleId }) => {
+    const [articleData, setArticleData] = useState(null);
+
+    useEffect(() => {
+        // Fetch the article data based on articleId
+        fetch(`/api/articles/${articleId}`)
+            .then(response => response.json())
+            .then(data => setArticleData(data))
+            .catch(error => console.error('Error fetching article:', error));
+    }, [articleId]);
+
+    return (
+        <div>
+            {articleData && (
+                <>
+                    <h1>{articleData.title}</h1>
+                    <p>Simplified Text: {articleData.simplified_text}</p>
+                    <div>
+                        <h3>Dictionary:</h3>
+                        <pre>{JSON.stringify(articleData.dictionary, null, 2)}</pre>
+                    </div>
+                </>
+            )}
+        </div>
+    );
+};
+
+export default ArticleComponent;
