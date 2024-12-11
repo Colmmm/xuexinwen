9d3c6bfd9149ecf46d93c4b8984713e1ebcfccbb
Colm Sam <colmwaikeat@gmail.com>
2024-12-11 16:03:10 +0000

    apiUrl for local setup needs to be :5000/api not just :5000

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 const isProduction = import.meta.env.VITE_PRODUCTION === 'true';
 
 const config = {
-  apiUrl: isProduction ? '/api' : 'http://localhost:5000',
+  apiUrl: isProduction ? '/api' : 'http://localhost:5000/api',
 };
 
 export default config;
